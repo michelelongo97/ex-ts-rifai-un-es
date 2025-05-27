f2a4bffe57fa078eb1698e0e4683bed1699e8ae7
michelelongo97 <m.longo7x@gmail.com>
2025-05-27 12:31:37 +0200

    feat: add typescript

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
+import dayjs from "dayjs";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -11,3 +12,62 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   
   </div>
 `;
+
+type Ricetta = {
+  id: number;
+  name: string;
+  userId: number;
+};
+
+type Chef = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  userId: number;
+  birthDate: string;
+};
+
+async function getChefBirthday(id: number): Promise<string> {
+  try {
+    const ricettaResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+    if (!ricettaResponse.ok) {
+      throw new Error(
+        `Errore HTTP ${ricettaResponse.status}: ${ricettaResponse.statusText}`
+      );
+    }
+    const ricetta: Ricetta = await ricettaResponse.json();
+
+    const chefResponse = await fetch(
+      `https://dummyjson.com/users/${ricetta.userId}`
+    );
+    if (!chefResponse.ok) {
+      throw new Error(
+        `Errore HTTP ${chefResponse.status}: ${chefResponse.statusText}`
+      );
+    }
+    const chef: Chef = await chefResponse.json();
+    const newDate: string = dayjs(chef.birthDate).format("DD/MM/YYYY");
+    return newDate;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`${error.message}`);
+    } else {
+      throw new Error("Errore sconsociuto");
+    }
+  }
+}
+
+(async (): Promise<void> => {
+  try {
+    const birthday: string = await getChefBirthday(1);
+    console.log(birthday);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("errore nel recupero dati", error);
+    } else {
+      console.error("Errore sconosciuto", error);
+    }
+  } finally {
+    console.log("fine");
+  }
+})();
